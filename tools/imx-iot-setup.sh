ff77306ff28fb6383d1f85c58d3dc3360e3ad53e
Haoran Wang <elven.wang@nxp.com>
2021-10-15 17:53:43 +0800

    fixed wrong build folder after rename the repo name
    Signed-off-by: Haoran Wang <elven.wang@nxp.com>

--- tools/imx-iot-setup.sh
+++ tools/imx-iot-setup.sh
@@ -4,7 +4,7 @@ fi
 EULA=$EULA DISTRO=$DISTRO MACHINE=$MACHINE . ./imx-setup-release.sh -b $@
 
 echo "# layers for i.MX IoT for MATTER & OpenThread Broader Router" >> conf/bblayers.conf
-echo "BBLAYERS += \"\${BSPDIR}/sources/meta-iot\"" >> conf/bblayers.conf
+echo "BBLAYERS += \"\${BSPDIR}/sources/meta-matter\"" >> conf/bblayers.conf
 echo "IMAGE_INSTALL_append += \" boost boost-dev boost-staticdev \"" >> conf/local.conf
 
 echo ""
