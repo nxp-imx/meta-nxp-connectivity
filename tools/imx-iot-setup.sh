e565bfd1f834f233acbf80e1d198923f5a09933a
Haoran Wang <elven.wang@nxp.com>
2022-06-29 13:58:35 +0000

    Apply Linux kernel configs for OTBR
    Openthread border router need some firewall and ipset defconfig
    enabled. Modify setup script to apply the config into linux-imx recipes.
    
    TEST: MACHINE=imx8mmevk DISTRO=fsl-imx-xwayland source
    sources/meta-matter/tools/imx-iot-setup.sh bld-imx8mm
    
    MACHINE=imx6ullevk DISTRO=fsl-imx-xwayland source
    sources/meta-matter/tools/imx-iot-setup.sh bld-imx6ull
    
    Signed-off-by: Haoran Wang <elven.wang@nxp.com>

--- tools/imx-iot-setup.sh
+++ tools/imx-iot-setup.sh
@@ -1,11 +1,20 @@
 if [ "$MACHINE" = "" ]; then
     MACHINE=imx8mmevk
 fi
+
+cd sources/meta-imx/
+true | git apply ../meta-matter/tools/patches/0001-Apply-Matter-enhancement-change-for-linux-imx.patch --check
+
+if [ $? == 0 ]; then
+    git apply ../meta-matter/tools/patches/0001-Apply-Matter-enhancement-change-for-linux-imx.patch
+fi
+cd ../..
+
 EULA=$EULA DISTRO=$DISTRO MACHINE=$MACHINE . ./imx-setup-release.sh -b $@
 
 echo "# layers for i.MX IoT for MATTER & OpenThread Broader Router" >> conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-matter\"" >> conf/bblayers.conf
-echo "IMAGE_INSTALL_append += \" boost boost-dev boost-staticdev \"" >> conf/local.conf
+echo "IMAGE_INSTALL:append += \" boost boost-dev boost-staticdev \"" >> conf/local.conf
 
 echo ""
 echo "Now you can use below command to generate your image:"
