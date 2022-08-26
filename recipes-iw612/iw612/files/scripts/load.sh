042b1ae7b4126c0adc58113b2d7acc332230bf6b
David Lin <yu-hao.lin@nxp.com>
2022-08-26 11:17:51 +0800

    Loeaded BT/15.4 firmware in load.sh.
    Signed-off-by: David Lin <yu-hao.lin@nxp.com>

--- recipes-iw612/iw612/files/scripts/load.sh
+++ recipes-iw612/iw612/files/scripts/load.sh
@@ -8,5 +8,8 @@ cwd=$PWD
 cd /usr/share/nxp_iw612/bin_sdw61x
 insmod mlan.ko
 insmod sdw61x.ko mod_para=config/wifi_mod_para.conf
+sleep 1
+./fw_loader_imx_lnx /dev/ttymxc2 115200 0 /lib/firmware/nxp/uartspi_n61x_v1.bin 3000000
+sleep 1
 
 cd $cwd
