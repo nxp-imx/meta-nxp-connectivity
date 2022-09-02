3bb933640ccdbb5ea6230722b7faba0f3a611af7
David Lin <yu-hao.lin@nxp.com>
2022-09-02 16:07:33 +0800

    Added script/configuration files for pairing.
    Signed-off-by: David Lin <yu-hao.lin@nxp.com>

---
+++ recipes-iw612/iw612/files/scripts/pairing.sh
@@ -0,0 +1,21 @@
+type=help
+
+if [ $# -gt 0 ]; then
+	source $1
+fi
+
+case "$type" in
+onnetwork)
+	chip-tool pairing $type $nodeid $pincode
+	;;
+ble-thread)
+	otbr_dataset=(`ot-ctl dataset active -x`)
+	chip-tool pairing $type $nodeid hex:${otbr_dataset//[$'\r\n']} $pincode $discriminator
+	;;
+help)
+	echo "Usage:"
+	echo "./pairing.sh pair_mw320.conf/pair_k32w.conf"
+	echo "pair_mw320.conf: configuration file for pairing MW320"
+	echo "pair_k32w.conf: configuration file for pairing K32W"
+	;;
+esac
