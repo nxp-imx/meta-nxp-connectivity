c4e80bea163bb11ec187565f3e221c6bc1db980a
David Lin <yu-hao.lin@nxp.com>
2022-10-05 15:30:42 +0800

    Corrected binding endpoint of light node.
    Signed-off-by: David Lin <yu-hao.lin@nxp.com>

--- recipes-iw612/iw612/files/scripts/binding.sh
+++ recipes-iw612/iw612/files/scripts/binding.sh
@@ -15,4 +15,4 @@ switch_nodeid=$1
 light_nodeid=$2
 
 chip-tool accesscontrol write acl '[{"fabricIndex": 1, "privilege": 5, "authMode": 2, "subjects": [112233], "targets": null },{"fabricIndex": 1, "privilege": 3, "authMode": 2, "subjects": ['$switch_nodeid'], "targets": null }]' $light_nodeid 0
-chip-tool binding write binding '[{"fabricIndex": 1, "node":'$light_nodeid', "endpoint": 1, "cluster": 6}]' $switch_nodeid 1 
+chip-tool binding write binding '[{"fabricIndex": 1, "node":'$light_nodeid', "endpoint": 2, "cluster": 6}]' $switch_nodeid 1
