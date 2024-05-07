685ece7000ad673cc22ea9ac0b8b1377a45034f0
Haoran Wang <elven.wang@nxp.com>
2024-05-07 09:59:48 +0800

    MATTER-2362-2 Update the imx-matter-setup script
    Since the meta-matter included into L6.6.23 Q2 BSP, so remove these
    layers first before config matter release layers.
    
    Also this patch refined the setup script.
    
    Signed-off-by: Haoran Wang <elven.wang@nxp.com>
    Change-Id: Icdaadefd4268c42da2910c5ff429df83396b4f1a
    Reviewed-on: http://androidsource.nxp.com/project/24812
    Reviewed-by: Xuegang Liu <xuegang.liu@nxp.com>

--- tools/imx-matter-setup.sh
+++ tools/imx-matter-setup.sh
@@ -16,12 +16,14 @@ if [ "$OT_RCP_BUS" = "UART" ]; then
     echo "OT_RCP_BUS = \"UART\"" >> conf/local.conf
 fi
  
-echo "# layers for i.MX IoT for MATTER & OpenThread Border Router" >> conf/bblayers.conf
+echo "# layers for i.MX MATTER & OpenThread Border Router" >> conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-connectivity/meta-nxp-matter-baseline\"" >> conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-connectivity/meta-nxp-matter-advanced\"" >> conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-connectivity/meta-nxp-openthread\"" >> conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-connectivity/meta-nxp-otbr\"" >> conf/bblayers.conf
 echo "BBLAYERS += \"\${BSPDIR}/sources/meta-nxp-connectivity/meta-nxp-connectivity-examples\"" >> conf/bblayers.conf
+
+sed '/meta-matter/d' "conf/bblayers.conf" > temp && mv temp "conf/bblayers.conf"
 
 echo ""
 echo "Now you can use below command to generate your image:"
@@ -31,6 +33,4 @@ echo "    $ bitbake imx-image-multimedia"
 echo "======================================================="
 echo "If you want to generate SDK, please use:"
 echo "    $ bitbake imx-image-core -c populate_sdk"
-echo "To exit Matter build environment, please:"
-echo "    $ deactivate"
 echo ""
